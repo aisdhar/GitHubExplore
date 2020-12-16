2c63995f16d4237ad5093d712bc17e0731d3df70
aisdhar <75646664+aisdhar@users.noreply.github.com>
2020-12-16 18:50:59 -0500

    Printing Numbers first commit
    second file test2.java

---
+++ ExploreGitHub/src/Test2.java
@@ -0,0 +1,13 @@
+
+public class Test2 {
+	public static void main(String[] args) {
+
+		int ar[] = { 10, 55, 983, 444, 123, 75, 16 };
+
+		for (int i = 0; i < ar.length; i++) {
+			System.out.println(ar[i]);
+		}
+
+	}
+
+}
