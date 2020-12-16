8cfd8bf9966413e0588174317ddfd8e1c0ce88ac
aisdhar <75646664+aisdhar@users.noreply.github.com>
2020-12-16 18:55:25 -0500

    Test2 file changed the inputs
    test2.java on new branch Test2file

--- ExploreGitHub/src/Test2.java
+++ ExploreGitHub/src/Test2.java
@@ -2,7 +2,7 @@
 public class Test2 {
 	public static void main(String[] args) {
 
-		int ar[] = { 10, 55, 983, 444, 123, 75, 16 };
+		int ar[] = { 1, 2, 3, 4, 6,7  };
 
 		for (int i = 0; i < ar.length; i++) {
 			System.out.println(ar[i]);
