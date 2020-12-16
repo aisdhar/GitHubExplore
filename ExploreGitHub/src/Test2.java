60295a15316e9d899e4c62586635dc2bd62187a1
aisdhar <75646664+aisdhar@users.noreply.github.com>
2020-12-16 18:58:36 -0500

    printing statement is changed test2file
    Test2.java

--- ExploreGitHub/src/Test2.java
+++ ExploreGitHub/src/Test2.java
@@ -2,10 +2,10 @@
 public class Test2 {
 	public static void main(String[] args) {
 
-		int ar[] = { 1, 2, 3, 4, 6,7  };
+		int ar[] = { 10, 20, 30, 40, 60, 70  };
 
 		for (int i = 0; i < ar.length; i++) {
-			System.out.println(ar[i]);
+			System.out.println("The values in the array are "+ ar[i]);
 		}
 
 	}
