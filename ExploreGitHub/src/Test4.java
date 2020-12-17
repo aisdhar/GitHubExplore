0c800bc4e4adcce1ab88657de37b2ca80200d500
aisdhar <75646664+aisdhar@users.noreply.github.com>
2020-12-16 19:05:32 -0500

    test2 file >>> changed the inputs
    test2.java

--- ExploreGitHub/src/Test4.java
+++ ExploreGitHub/src/Test4.java
@@ -2,9 +2,9 @@
 public class Test4 {
 
 	public static void main(String[] args) {
-		int a = 10, b = 20, c = 30;
+		int a = 10, b = 20;
 		int sum;
-		sum = a + b + c;
+		sum = a + b;
 		System.out.println(sum);
 
 	}
